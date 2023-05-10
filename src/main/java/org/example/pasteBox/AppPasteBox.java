cd66de9e49b3218cd45cf79f49d2ccbc3e27e1f6
SquintEyed <gerdmm1@gmail.com>
2023-05-10 22:30:15 +0300

    app init commit

---
+++ src/main/java/org/example/pasteBox/AppPasteBox.java
@@ -0,0 +1,13 @@
+package org.example.pasteBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPasteBox {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AppPasteBox.class, args);
+    }
+}
